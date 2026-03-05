6e29f1e6f45340b20f276f710d912c54ee3ff2d8
Yuni Nguyen <yuninguyen.it@gmail.com>
2026-03-05 11:20:18 +0700

    Update HasTrackerSchema.php

--- app/Filament/Resources/Traits/HasTrackerSchema.php
+++ app/Filament/Resources/Traits/HasTrackerSchema.php
@@ -16,6 +16,10 @@ use Illuminate\Database\Eloquent\Builder;
 
 trait HasTrackerSchema
 {
+    // FIX #8: Dùng chung $usStates từ HasUsStates thay vì khai báo lại ở đây.
+    // HasAccountSchema cũng dùng cùng trait này → chỉ cần sửa 1 chỗ duy nhất.
+    use HasUsStates;
+    
     /**
      * Scope lọc các bản ghi đang ở trạng thái có thể rút tiền
      */
@@ -378,58 +382,7 @@ trait HasTrackerSchema
                                 Forms\Components\Select::make('state')
                                     ->label('State (US)')
                                     ->searchable()
-                                    
-                                        'AL' => 'Alabama',
-                                        'AK' => 'Alaska',
-                                        'AZ' => 'Arizona',
-                                        'AR' => 'Arkansas',
-                                        'CA' => 'California',
-                                        'CO' => 'Colorado',
-                                        'CT' => 'Connecticut',
-                                        'DE' => 'Delaware',
-                                        'FL' => 'Florida',
-                                        'GA' => 'Georgia',
-                                        'HI' => 'Hawaii',
-                                        'ID' => 'Idaho',
-                                        'IL' => 'Illinois',
-                                        'IN' => 'Indiana',
-                                        'IA' => 'Iowa',
-                                        'KS' => 'Kansas',
-                                        'KY' => 'Kentucky',
-                                        'LA' => 'Louisiana',
-                                        'ME' => 'Maine',
-                                        'MD' => 'Maryland',
-                                        'MA' => 'Massachusetts',
-                                        'MI' => 'Michigan',
-                                        'MN' => 'Minnesota',
-                                        'MS' => 'Mississippi',
-                                        'MO' => 'Missouri',
-                                        'MT' => 'Montana',
-                                        'NE' => 'Nebraska',
-                                        'NV' => 'Nevada',
-                                        'NH' => 'New Hampshire',
-                                        'NJ' => 'New Jersey',
-                                        'NM' => 'New Mexico',
-                                        'NY' => 'New York',
-                                        'NC' => 'North Carolina',
-                                        'ND' => 'North Dakota',
-                                        'OH' => 'Ohio',
-                                        'OK' => 'Oklahoma',
-                                        'OR' => 'Oregon',
-                                        'PA' => 'Pennsylvania',
-                                        'RI' => 'Rhode Island',
-                                        'SC' => 'South Carolina',
-                                        'SD' => 'South Dakota',
-                                        'TN' => 'Tennessee',
-                                        'TX' => 'Texas',
-                                        'UT' => 'Utah',
-                                        'VT' => 'Vermont',
-                                        'VA' => 'Virginia',
-                                        'WA' => 'Washington',
-                                        'WV' => 'West Virginia',
-                                        'WI' => 'Wisconsin',
-                                        'WY' => 'Wyoming',
-                                    }),
+                                    ->options(self::$usStates),
                             ]),
                         Forms\Components\Textarea::make('note')
                             ->label('Note')
